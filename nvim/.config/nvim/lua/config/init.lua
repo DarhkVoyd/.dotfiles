8f3050b3a7f4005d6327739ad3df5291f2030019
D V <divyadityasnaruka@gmail.com>
2024-04-03 01:33:57 +0530

    migrate to lazy.nvim

--- nvim/.config/nvim/lua/config/init.lua
+++ nvim/.config/nvim/lua/config/init.lua
@@ -1,11 +1,14 @@
 require("config.set")
 require("config.remap")
 
+require("config.lazy_init")
+
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
 local whitespace_cleanup_group = augroup('WhitespaceCleanup', {})
 local yank_highlight_group = augroup('YankHighlight', {})
+local lsp_attach_group = augroup('LspAttach', {})
 
 function R(name)
     require("plenary.reload").reload_module(name)
@@ -26,6 +29,23 @@ autocmd("BufWritePre", {
     group = whitespace_cleanup_group,
     pattern = "*",
     command = [[%s/\s\+$//e]],
+})
+
+autocmd('LspAttach', {
+    group = lsp_attach_group,
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    end
 })
 
 vim.g.netrw_browse_split = 0
