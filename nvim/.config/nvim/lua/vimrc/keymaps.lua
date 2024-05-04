831f0b5fab1127cc86bf65c56576f6a462131685
D V <divyadityasnaruka@gmail.com>
2024-05-04 15:16:46 +0530

    update chmod keymap and add un-chmod

--- nvim/.config/nvim/lua/vimrc/keymaps.lua
+++ nvim/.config/nvim/lua/vimrc/keymaps.lua
@@ -34,7 +34,8 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>ch", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>uch", "<cmd>!chmod -x %<CR>", { silent = true })
 
 -- vim.keymap.set(
 --     "n",
