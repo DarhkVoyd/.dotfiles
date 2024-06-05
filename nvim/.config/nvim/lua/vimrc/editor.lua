fa4a92f8b7835258f8c6e1cf469a647c8c5f4d83
D V <divyadityasnaruka@gmail.com>
2024-06-06 00:05:56 +0530

    copy to clipboard instead of appending

--- nvim/.config/nvim/lua/vimrc/editor.lua
+++ nvim/.config/nvim/lua/vimrc/editor.lua
@@ -30,7 +30,8 @@ vim.opt.background = "dark"
 
 vim.opt.backspace = "indent,eol,start"
 
-vim.opt.clipboard:append("unnamedplus")
+-- vim.opt.clipboard:append("unnamedplus")
+vim.opt.clipboard = "unnamedplus"
 
 vim.opt.splitright = true
 vim.opt.splitbelow = true
