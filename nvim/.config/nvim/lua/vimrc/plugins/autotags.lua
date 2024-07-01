1c306fefaf302bc28d9fd153f3c8f3cb35023380
D V <divyadityasnaruka@gmail.com>
2024-07-01 12:35:00 +0530

    fix nvim autotags deprecation warning

--- nvim/.config/nvim/lua/vimrc/plugins/autotags.lua
+++ nvim/.config/nvim/lua/vimrc/plugins/autotags.lua
@@ -1,7 +1,8 @@
 return {
-    "windwp/nvim-ts-autotag",
-    ft = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-    config = function()
-        require("nvim-ts-autotag").setup()
-    end,
+	"windwp/nvim-ts-autotag",
+	event = { "BufReadPre", "BufNewFile" },
+	ft = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+	config = function()
+		require("nvim-ts-autotag").setup()
+	end,
 }
